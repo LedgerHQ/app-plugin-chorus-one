844da57b6b6dcc6b31c4f46281e28c8842992b7f
Fynn <me@enriquefynn.com>
2024-06-18 21:22:11 -0300

    Lint .h

--- src/utils.h
+++ src/utils.h
@@ -1,5 +1,8 @@
 #pragma once
 
-void display_first_and_last_bytes(ethQueryContractUI_t *msg, uint8_t *address, int array_offset, int first_last_size);
+void display_first_and_last_bytes(ethQueryContractUI_t *msg,
+                                  uint8_t *address,
+                                  int array_offset,
+                                  int first_last_size);
 void display_uint_decimal(ethQueryContractUI_t *msg, uint8_t *number, size_t size);
 void display_hexa(ethQueryContractUI_t *msg, uint8_t *data, size_t length);
